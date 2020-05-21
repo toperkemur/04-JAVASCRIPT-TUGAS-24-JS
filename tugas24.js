3a89bc092ab553d24fcc891878222fbc40f14659
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-05-22 01:41:10 +0700

    Initial Commit
    Menambahkan tugas24.js

---
+++ tugas24.js
@@ -0,0 +1,6 @@
+var arr = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66 ];
+console.log("Sebelumnya :", arr.join(","));
+arr = arr.sort();
+console.log("Ascending : ", arr.join(","));
+arr = arr.reverse();
+console.log("Descending :", arr.join(","));
